e89e0c7839c9750a5025c23e879bfd94e2c44d42
NDanczuk <nicolasdanc@gmail.com>
2025-04-13 22:08:43 -0300

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './App.css';
+import * as bootstrap from 'bootstrap';
+import { Tooltip, Toast, Popover } from 'bootstrap';
+
+function App() {
+  return (
+    <>
+      <div>TESTE</div>
+    </>
+  );
+}
+
+export default App;
